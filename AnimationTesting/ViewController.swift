1b16edf381a10fba70508f013e8f73a3b771c569
Mitchell Porter <mitchellporter@gmail.com>
2016-02-11 21:13:43 -0800

    JNWSpringAnimation will not work with autolayout constraints

--- AnimationTesting/ViewController.swift
+++ AnimationTesting/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JNWSpringAnimation
 
 class ViewController: UIViewController {
     
@@ -31,29 +32,55 @@ class ViewController: UIViewController {
     }
     
     func firstAnimation() {
-        self.redBoxCenterYContraint.constant += CGFloat(100)
-        UIView.animateWithDuration(1.0, delay: 0.0, options: [.CurveEaseOut], animations: { () -> Void in
-            self.redBox.superview?.layoutIfNeeded()
-            
-            }) { (complete) -> Void in
-                self.secondAnimation()
-        }
         
-        UIView.animateWithDuration(1.0, delay: 0.0, options: [], animations: { () -> Void in
-            let value = CGFloat(M_PI)
-            self.redBox.transform = CGAffineTransformMakeRotation(value)
-
-            }) { (complete) -> Void in
-        }
+        let animation = JNWSpringAnimation(keyPath: "position.y")
+        animation.fromValue = self.redBox.layer.position.y
+        animation.toValue = self.redBox.layer.position.y + CGFloat(100.0)
+        animation.delegate = self
+        redBox.layer.addAnimation(animation, forKey: animation.keyPath)
+        
+        
+        
+        
+//        self.redBoxCenterYContraint.constant += CGFloat(100)
+//        UIView.animateWithDuration(1.0, delay: 0.0, options: [.CurveEaseOut], animations: { () -> Void in
+//            self.redBox.superview?.layoutIfNeeded()
+//            
+//            }) { (complete) -> Void in
+//                self.secondAnimation()
+//        }
+//        
+//        UIView.animateWithDuration(1.0, delay: 0.0, options: [], animations: { () -> Void in
+//            let value = CGFloat(M_PI)
+//            self.redBox.transform = CGAffineTransformMakeRotation(value)
+//
+//            }) { (complete) -> Void in
+//        }
     }
     
     func secondAnimation() {
-        self.redBoxCenterYContraint.constant = self.originalY
-        UIView.animateWithDuration(5.0, delay: 0.0, options: [.CurveEaseOut], animations: { () -> Void in
-            self.redBox.superview?.layoutIfNeeded()
-            }) { (complete) -> Void in
-                print(self.redBox.frame)
-        }
+        
+        let animation = JNWSpringAnimation(keyPath: "position.y")
+        animation.fromValue = self.redBox.layer.position.y
+        animation.toValue = self.redBox.layer.position.y - CGFloat(100.0)
+        redBox.layer.addAnimation(animation, forKey: animation.keyPath)
+        
+        
+        
+//        self.redBoxCenterYContraint.constant = self.originalY
+//        UIView.animateWithDuration(5.0, delay: 0.0, options: [.CurveEaseOut], animations: { () -> Void in
+//            self.redBox.superview?.layoutIfNeeded()
+//            }) { (complete) -> Void in
+//                print(self.redBox.frame)
+//        }
+    }
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        
+
+        redBoxCenterYContraint.constant = originalY - CGFloat(100.0)
+        
+        print("anim finished")
     }
 
     override func didReceiveMemoryWarning() {
